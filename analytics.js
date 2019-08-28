1857b2a79ce4f3032d5e28f412005827fc457458
Ann Savinkova <47611374+FinnaA@users.noreply.github.com>
2019-08-28 09:00:55 +0300

    Update analytics.js

--- analytics.js
+++ analytics.js
@@ -3,6 +3,11 @@
 window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
 ga('create', '208523347', 'auto');
 ga('send', 'pageview');
+ga('create', 'GA_MEASUREMENT_ID', 'auto');
+ga('set', 'appName', 'myAppName');
+// The `appName` field is now set, so
+// screenview hits don't need to include it.
+ga('send', 'screenview', {screenName: 'Home'});
 </script>
 <script async src='https://www.google-analytics.com/analytics.js'></script>
 <!-- End Google Analytics -->
